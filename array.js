1b113782c0adb54120fcab9b555a80e3ffceafea
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-06-08 13:09:06 -0400

    Add Array content

--- array.js
+++ array.js
@@ -2,12 +2,13 @@
 
 " Array part 2"
 var friends = ["steffie", "Bob", "Jim", "Pam", "Mike"];
-
-var ages =[23,34,45,35]
-
-var marriedMen =[false,true,true,false]
-
-
-"removes last elment, should read (steffie, bob, Jim, Pam)"
+//removes last element from array
 friends.pop();
 console.log(friends);
+var ages =[23,34,45,35]
+//adds 1 or more & back to original
+ages.push();
+console.log(ages);
+var marriedMen =[false,true,true,false]
+//removes 1st element
+console.log(marriedMen);
